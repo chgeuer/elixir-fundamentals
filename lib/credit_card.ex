fa51c96e07ebeee17d7e04e211d1aa48de8bbc23
Christian Geuer-Pollmann <chgeuer@microsoft.com>
2018-04-19 22:57:50 +0200

    Solved github

--- lib/credit_card.ex
+++ lib/credit_card.ex
@@ -2,8 +2,8 @@ defmodule CreditCard do
   # @enforce_keys [:expire_year, :type]
   defstruct [:expire_year, :type]
 
-  def process(%CreditCard{type: "discover"}), do: raise FunctionClauseError
-  def process(%CreditCard{expire_year: year}) when year < 2018, do: raise FunctionClauseError
+  def process(%CreditCard{type: "discover"}), do: raise(FunctionClauseError)
+  def process(%CreditCard{expire_year: year}) when year < 2018, do: raise(FunctionClauseError)
   def process(%CreditCard{}), do: "OK!"
-  def process(_), do: raise FunctionClauseError
+  def process(_), do: raise(FunctionClauseError)
 end
--- lib/github.ex
+++ lib/github.ex
@@ -1,6 +1,28 @@
 defmodule Github do
-  def org_url(_org_name), do: "TODO: org_url"
-  def repo_url(_owner_name, _repo_name), do: "TODO: repo_url"
-  def org(org_name), do: "TODO: org"
-  def repo(owner_name, repo_name), do: "TODO: repo"
+  def org_url(org_name) when is_binary(org_name), do: "#{api()}/orgs/#{org_name}"
+
+  def org_url(_), do: raise(FunctionClauseError)
+
+  def repo_url(owner_name, repo_name) when is_binary(owner_name) and is_binary(repo_name),
+    do: "#{api()}/repos/#{owner_name}/#{repo_name}"
+
+  def repo_url(_, _), do: raise(FunctionClauseError)
+
+  def org(org_name) when is_binary(org_name), do: org_name |> org_url |> fetch
+
+  def repo(owner_name, repo_name) when is_binary(owner_name) and is_binary(repo_name) do
+    repo_url(owner_name, repo_name)
+    |> fetch
+  end
+
+  defp fetch(url), do: url |> HTTPoison.get!() |> process_body
+
+  defp process_body(%{body: response_body, status_code: 200}),
+    do: {:ok, response_body |> Poison.decode!()}
+
+  defp process_body(%{body: response_body, status_code: _}),
+    do: {:error, response_body |> Poison.decode!()}
+
+  def api, do: Application.get_env(:elixir_fundamentals, :github_api)
+  def token, do: Application.get_env(:elixir_fundamentals, :github_token)
 end
